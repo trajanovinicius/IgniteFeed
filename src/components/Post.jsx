ba4287e77b5b3a9c26d70c5fcaa7c74bf58908f3
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2025-04-24 00:45:52 -0300

    finished post component and comment section

---
+++ src/components/Post.jsx
@@ -0,0 +1,47 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+	return (
+		<article className={styles.post}>
+			<header>
+				<div className={styles.author}>
+					<img
+						className={styles.avatar}
+						src="https://github.com/trajanovinicius.png"
+					/>
+					<div className={styles.authorInfo}>
+						<strong>Vinicius Trajano</strong>
+						<span>Web Developer</span>
+					</div>
+				</div>
+
+				<time title="23 de abril às 23:19" dateTime="2025-04-23">
+					Publicado há 1h
+				</time>
+			</header>
+			<div className={styles.content}>
+				<p>Fala galeraa 👋</p>
+				<p>
+					Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+					no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+				</p>
+				<p>
+					👉 <a href="">jane.design/doctorcare</a>
+				</p>
+				<p>
+					<a href="">#novoprojeto</a> <a href="">#nlw </a>{" "}
+					<a href="">#rocketseat </a>{" "}
+				</p>
+			</div>
+
+			<form className={styles.commentForm}>
+				<strong>Deixe seu feedback</strong>
+
+				<textarea placeholder="Deixe um comentário..." />
+				<footer>
+					<button type="submit">Publicar</button>
+				</footer>
+			</form>
+		</article>
+	);
+}
