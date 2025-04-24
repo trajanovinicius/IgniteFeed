cdd1f93134ab25352b000cd99d667eb72a80c95d
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2025-04-23 23:00:16 -0300

    finished sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilLine } from "phosphor-react";
+
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -9,12 +11,20 @@ export function Sidebar() {
 			/>
 
 			<div className={styles.profile}>
+				<img
+					className={styles.avatar}
+					src="https://github.com/trajanovinicius.png"
+				/>
+
 				<strong>Vinicius Trajano</strong>
 				<span>Web Developer</span>
 			</div>
 
 			<footer>
-				<a href="#">Editar Seu Perfil</a>
+				<a href="#">
+					<PencilLine size={20} />
+					Editar Seu Perfil
+				</a>
 			</footer>
 		</aside>
 	);
