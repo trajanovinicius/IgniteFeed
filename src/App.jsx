168ac5f16985349f520f0b618c2befeb88dee7bf
Rodolfo Henrique <viniciustrajanobs@gmail.com>
2025-04-29 21:25:21 -0300

    finish properties Post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+	{
+		id: 1,
+		author: {
+			avatarUrl: "https://github.com/trajanovinicius.png",
+			name: "Vinicius Trajano",
+			role: "Web Developer",
+		},
+		content: [
+			{ type: "paragraph", content: "Fala galeraa 👋" },
+			{
+				type: "paragraph",
+				content:
+					"Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+			},
+			{ type: "link", content: "jane.design/doctorcare" },
+		],
+		publishedAt: new Date("2025-04-29 21:00:00"),
+	},
+	{
+		id: 2,
+		author: {
+			avatarUrl: "https://github.com/maykbrito.png",
+			name: "Maiki Brito",
+			role: "Web Developer Educator",
+		},
+		content: [
+			{ type: "paragraph", content: "Fala galeraa 👋" },
+			{
+				type: "paragraph",
+				content:
+					"Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+			},
+			{ type: "link", content: "jane.design/doctorcare" },
+		],
+		publishedAt: new Date("2025-04-29 21:00:00"),
+	},
+];
+
 export function App() {
 	return (
 		<div>
@@ -14,14 +53,15 @@ export function App() {
 			<div className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<Post
-						author="Vinicius T"
-						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eaque explicabo rerum. Optio numquam consequuntur perspiciatis rerum corporis quidem esse cumque dignissimos vero aspernatur sint, quisquam obcaecati iure pariatur quos."
-					/>
-					<Post
-						author="Bob D"
-						content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero eaque explicabo rerum. Optio numquam consequuntur perspiciatis rerum corporis quidem esse cumque dignissimos vero aspernatur sint, quisquam obcaecati iure pariatur quos."
-					/>
+					{posts.map((post) => {
+						return (
+							<Post
+								author={post.author}
+								content={post.content}
+								publishedAt={post.publishedAt}
+							/>
+						);
+					})}
 				</main>
 			</div>
 		</div>
