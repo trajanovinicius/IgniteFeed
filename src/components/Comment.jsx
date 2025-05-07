1c8bc947d7333e0cb4182495c13871f3991b0450
Vinicius Trajano <viniciustrajanobs@gmail.com>
2025-05-07 17:17:54 -0300

    feat: adding like counter to comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import { Avatar } from "./Avatar";
+import { useState } from "react";
 
 export function Comment({ content, onDeleteComment }) {
+	const [likeCount, setLikeCount] = useState(0);
+
 	function handleDeleteComment() {
 		onDeleteComment(content);
+	}
+
+	function handleLikeComment() {
+		setLikeCount(likeCount + 1);
 	}
 
 	return (
@@ -33,9 +40,9 @@ export function Comment({ content, onDeleteComment }) {
 				</div>
 
 				<footer>
-					<button>
+					<button onClick={handleLikeComment}>
 						<ThumbsUp />
-						Apludir <span>20</span>
+						Apludir <span>{likeCount}</span>
 					</button>
 				</footer>
 			</div>
