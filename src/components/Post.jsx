229c77672aaa71b5f89437b44627d879e9acddde
Vinicius Trajano <viniciustrajanobs@gmail.com>
2025-05-07 17:08:28 -0300

    feat: adding form validation

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,7 +31,12 @@ export function Post({ author, publishedAt, content }) {
 	}
 
 	function handleNewCommentChange() {
+		event.target.setCustomValidity("");
 		setNewCommentText(event.target.value);
+	}
+
+	function handleNewCommentInvalid() {
+		event.target.setCustomValidity("Este campo é obrigatório!");
 	}
 
 	function deleteComment(commentToDelete) {
@@ -40,6 +45,8 @@ export function Post({ author, publishedAt, content }) {
 		});
 		setComments(commentsWithoutDeletedOne);
 	}
+
+	const isNewCommentEmpty = newCommentText.length === 0;
 
 	return (
 		<article className={styles.post}>
@@ -81,9 +88,13 @@ export function Post({ author, publishedAt, content }) {
 					placeholder="Deixe um comentário..."
 					value={newCommentText}
 					onChange={handleNewCommentChange}
+					onInvalid={handleNewCommentInvalid}
+					required
 				/>
 				<footer>
-					<button type="submit">Publicar</button>
+					<button type="submit" disabled={isNewCommentEmpty}>
+						Publicar
+					</button>
 				</footer>
 			</form>
 
